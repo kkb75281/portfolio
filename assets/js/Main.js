3e6bb27fcdabdc0813714966e0cefd2c09b67bee
kkb75281 <kkb75281@gmail.com>
2022-12-14 13:15:40 +0900

    2022.12.14

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -16,27 +16,111 @@ document.querySelector(".top").addEventListener("click", () => {
 });
 
 // 스크롤 오버했을때
-const scrollsite = document.querySelector(".hover");
-const presite = document.querySelectorAll(".presite");
+const hover1 = document.querySelector(".h1");
+const hover2 = document.querySelector(".h2");
+const hover3 = document.querySelector(".h3");
+const hover4 = document.querySelector(".h4");
+const hover5 = document.querySelector(".h5");
+const hover6 = document.querySelector(".h6");
+const hover7 = document.querySelector(".h7");
+const presite1 = document.querySelector(".ps1");
+const presite2 = document.querySelector(".ps2");
+const presite3 = document.querySelector(".ps3");
+const presite4 = document.querySelector(".ps4");
+const presite5 = document.querySelector(".ps5");
+const presite6 = document.querySelector(".ps6");
+const presite7 = document.querySelector(".ps7");
 
-// const img = new Image()
-// img.src = "./assets/img/site1.jpg"
-// console.log(img.height)
+const s1 = gsap.timeline({ paused: true });
+const s2 = gsap.timeline({ paused: true });
+const s3 = gsap.timeline({ paused: true });
+const s4 = gsap.timeline({ paused: true });
+const s5 = gsap.timeline({ paused: true });
+const s6 = gsap.timeline({ paused: true });
+const s7 = gsap.timeline({ paused: true });
 
-const TL = gsap.timeline({ paused: true });
-// const imgHeight = gsap.getProperty(".presite","naturalheight")
-
-TL.to(presite, {
+s1.to(presite1, {
   scale: 1.0,
   duration: 8,
   stagger: 0.1,
-  // ease: "back.out(1.7)",
-  y: -1360,
+  y: -1950,
 });
 
-scrollsite.addEventListener("mouseenter", () => {
-  TL.play();
+s2.to(presite2, {
+  scale: 1.0,
+  duration: 8,
+  stagger: 0.1,
+  y: -620,
 });
-scrollsite.addEventListener("mouseout", () => {
-  TL.reverse();
+
+s3.to(presite3, {
+  scale: 1.0,
+  duration: 8,
+  stagger: 0.1,
+  y: -1660,
+});
+
+s4.to(presite4, {
+  scale: 1.0,
+  duration: 8,
+  stagger: 0.1,
+  x: -2230,
+});
+
+s5.to(presite5, {
+  scale: 1.0,
+  duration: 8,
+  stagger: 0.1,
+  y: -1220,
+});
+
+s6.to(presite6, {
+  scale: 1.0,
+  duration: 8,
+  stagger: 0.1,
+  x: -2230,
+});
+
+// s7.to(presite7, {
+//   scale: 1.0,
+//   duration: 8,
+//   stagger: 0.1,
+//   y: -1360,
+// });
+
+hover1.addEventListener("mouseenter", () => {
+  s1.play();
+});
+hover1.addEventListener("mouseout", () => {
+  s1.reverse();
+});
+hover2.addEventListener("mouseenter", () => {
+  s2.play();
+});
+hover2.addEventListener("mouseout", () => {
+  s2.reverse();
+});
+hover3.addEventListener("mouseenter", () => {
+  s3.play();
+});
+hover3.addEventListener("mouseout", () => {
+  s3.reverse();
+});
+hover4.addEventListener("mouseenter", () => {
+  s4.play();
+});
+hover4.addEventListener("mouseout", () => {
+  s4.reverse();
+});
+hover5.addEventListener("mouseenter", () => {
+  s5.play();
+});
+hover5.addEventListener("mouseout", () => {
+  s5.reverse();
+});
+hover6.addEventListener("mouseenter", () => {
+  s6.play();
+});
+hover6.addEventListener("mouseout", () => {
+  s6.reverse();
 });
